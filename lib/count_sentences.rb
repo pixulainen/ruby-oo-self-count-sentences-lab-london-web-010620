2404ced06000060eb5a859a42547f8d016e27921
pixulainen <george.apetrei@gmail.com>
2020-01-09 22:34:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+   if self[-1] == "."
+     true
+   else
+      false
+   end
+
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+       false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+       false
+    end
   end
 
-  def count_sentences
-
+  def count_sentences()
+    #uniq
+    self.split(/[.?!]+/).grep(/\S/).count
   end
+
 end
